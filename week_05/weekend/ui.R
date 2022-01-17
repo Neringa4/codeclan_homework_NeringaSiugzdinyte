8c6a1361ded0077b465667a58ccc8e7e0838bfd1
Neringa4 <94367447+Neringa4@users.noreply.github.com>
2022-01-17 19:04:51 +0000

    Update ui.R

--- week_05/weekend/ui.R
+++ week_05/weekend/ui.R
@@ -10,50 +10,52 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput(
-        inputId = "publisher",
-        label = h3("Publisher"),
-        choices = c("All", publishers)
-      ),
-      selectInput(
-        inputId = "genre",
-        label = h3("Genre"),
-        choices = c("All", genres)
-      ),
-      selectInput(
-        inputId = "platform",
-        label = h3("Platform"),
-        choices = c("All" ,platforms)
-      ),
-      align = "center",
-      style = "font-size:17px;"
-    ),
-    
-    mainPanel(
-      tabsetPanel(
-        type = "tabs",
-        tabPanel(
-          "Ratings",
-          
-          titlePanel(h1("Average Game Ratings Over Time",
-                        align = "center")),
-          
-          br(),
-          
-          column(
-            width = 12,
-            plotOutput("games_Plot")
-          )
+        selectInput(
+          inputId = "publisher",
+          label = h3("Publisher"),
+          choices = c("All", publishers)
         ),
-        tabPanel(
-          "Top Games",
-          
-          titlePanel(h1("Top 10 Highest Rated Games",
-                        align = "center")),
-          
-          br(),
-          
-          tableOutput("games_table")
-          
+        selectInput(
+          inputId = "genre",
+          label = h3("Genre"),
+          choices = c("All", genres)
+        ),
+        selectInput(
+          inputId = "platform",
+          label = h3("Platform"),
+          choices = c("All" ,platforms)
+        ),
+        align = "center",
+        style = "font-size:17px;"
+      ),
+      
+      mainPanel(
+        tabsetPanel(
+          type = "tabs",
+          tabPanel(
+            "Ratings",
+            
+            titlePanel(h1("Average Game Ratings Over Time",
+                          align = "center")),
+            
+            br(),
+            
+            column(
+              width = 12,
+              plotOutput("games_Plot")
+            )
+          ),
+          tabPanel(
+            "Top Games",
+            
+            titlePanel(h1("Top 10 Highest Rated Games",
+                          align = "center")),
+            
+            br(),
+            
+            tableOutput("games_table")
+            
+          )
         )
       )
     )
